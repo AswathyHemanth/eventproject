ca3954b1f1199fc01b857fc968f2d52f17f5a12c
AswathyHemanth <iaswathy.h@gmail.com>
2024-09-01 11:20:16 +0300

    created basic structure

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,78 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class = "bigblock">
+    <div class = "event">
+<div class="event1">
+    <h1>Event at Asiantown</h1>
+    <button class = "booknow1"> BOOK NOW </button>
+
+</div>
+
+<div class="event2">
+    <h1>Event at Asiantown</h1>
+    <button class = "booknow2"> BOOK NOW </button>
+
+</div>
+
+<div class="event3">
+    <h1>Event at Asiantown</h1>
+    <button class = "booknow3"> BOOK NOW </button>
+
+</div>
+<div class="event4">
+    <h1>Event at Asiantown</h1>
+    <button class = "booknow4"> BOOK NOW </button>
+
+</div>
+</div>
+</div>
+
+<style>
+
+.bigblock{
+    background-color: black;
+}
+ .event1{
+    height : 300px;
+    width : 300px;
+    border : 5px;
+    display : inline-block;
+    background-color : #e5e5fa;
+    flex : 1px;
+    
+ }
+ .event2{
+    height : 300px;
+    width : 300px;
+    border : 10px;
+    display : inline-block;
+    background-color : #e5e5fa;
+    flex : 1px;
+    
+ }
+ .event3{
+    height : 300px;
+    width : 300px;
+    border : 5px;
+    display : inline-block;
+    background-color : #e5e5fa;
+    flex : 1px;
+    
+    
+ }
+ .event4{
+    height : 300px;
+    width : 300px;
+    border : 5px;
+    display : inline-block;
+    background-color : #e5e5fa;
+    flex : 1px;
+    
+ }
+
+ .booknow1{
+         background-color: rgb(91, 129, 231);
+         flex: 1px;
+
+    
+ }
+
+</style>
