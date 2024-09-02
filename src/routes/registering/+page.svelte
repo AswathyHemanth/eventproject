e4fa81b0d0d835800143f8ebd969a54027964d66
AswathyHemanth <iaswathy.h@gmail.com>
2024-09-02 09:37:56 +0300

    pages created

---
+++ src/routes/registering/+page.svelte
@@ -0,0 +1,50 @@
+<script>
+
+let formData = {
+    name: '',
+    email: '',
+    mobile: '',
+    password: '',
+    confirm_password:'',
+
+};
+
+
+function handleSubmit(event) {
+    event.preventDefault();
+
+    formData = {
+        name: '',
+        email: '',
+        password: ''
+    };
+}
+</script>      
+<div class = "reg"
+<form method="POST" action="//create" on:submit={handleSubmit}>
+<label>
+    Name:
+    <input type="text" bind:value={formData.name} required />
+</label>
+<br />
+<label>
+    Email:
+    <input type="email" bind:value={formData.email} required />
+</label>
+<br /><label>
+    Mobile:
+    <input type="mobile" bind:value={formData.mobile} required />
+</label>
+<br />
+<label>
+    Password:
+    <input type="password" bind:value={formData.password} required />
+</label>
+<br />
+<label>
+    Confirm Password:
+    <input type="confirm_password" bind:value={formData.confirm_password} required />
+</label>
+<br />
+<button type="submit">Register</button>
+</form>
